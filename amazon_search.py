845d270af0112a05830cb3fed07f9775772b941c
maazmohmed <52424450+maazmohmed@users.noreply.github.com>
2021-04-26 08:43:20 +0530

    Add files via upload

---
+++ amazon_search.py
@@ -0,0 +1,9 @@
+from autoscraper import AutoScraper
+amazon_url="https://www.amazon.in/s?k=iphones"
+
+wanted_list=["₹58,400","New Apple iPhone 11 (128GB) - Black"]
+
+scraper=AutoScraper()
+result=scraper.build(amazon_url,wanted_list)
+
+print(scraper.get_result_similar(amazon_url,grouped=True))
